f7dc5e338fc984b0d28c84a9b6516a4478cf641f
LUOWEIMING <weiming.luo@microworld.co.jp>
2026-05-14 16:28:57 +0900

    Feat: Chapter 13 Improving Accessibility

---
+++ dashboard/starter-example/eslint.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import next from 'eslint-config-next';
+ 
+const eslintConfig = defineConfig([
+  ...next,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+]);
+ 
+export default eslintConfig;
